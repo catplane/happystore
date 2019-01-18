3e3f7f52cf6068ef5c8e5cb50e63f5e8f833fe3c
catplane <nihaoyawoshixixida@gmail.com>
2019-01-18 21:02:21 +0800

    定义用户

--- meido_mall/meido_mall/settings/dev.py
+++ meido_mall/meido_mall/settings/dev.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -191,3 +192,6 @@ REST_FRAMEWORK = {
     # 异常处理
     'EXCEPTION_HANDLER': 'meiduo_mall.utils.exceptions.exception_handler',
 }
+
+
+AUTH_USER_MODEL = 'users.User'
